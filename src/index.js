9a8b736acdd67f1192899056b9f6d6c7c908c532
Ian Nastajus <ian.nastajus@gmail.com>
2018-03-06 15:29:57 -0500

    React.createElement("", {}, "")  and ReactDOM.render(what, where); // :D

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-React.createElement();
-React.render(what, where); //nice
+const what = React.createElement("h1", {}, "hello react!");
+//1st arg: any type of HTML tag will work here in quotes.
+//2nd arg: the OPTIONS for that element:  classname, id, "data attribs" etc.
+//3rd arg: innerText or Children.
+const where = document.getElementById("root");
+
+ReactDOM.render(what, where); //nice
